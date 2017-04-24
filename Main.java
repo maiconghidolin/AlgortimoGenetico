ed3addfb2bd5b5b1f06a39dbcf1498c60b5a4843
wagnerfrana94 <wagnerfrana@hotmail.com>
2017-04-24 13:49:44 -0300

    Main final

--- Main.java
+++ Main.java
@@ -3,64 +3,61 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Collections;
+import java.net.*;
+import java.io.*;
 
 class Main {
 
-    private static final int quantidadeIteracoes = 1;
+    private static final int quantidadeIteracoes = 20;
 
     public static void main(String args[]){
 		
 		double valorFitness[];
 		
-        try {
+        	try {
 			
 			if (args != null) {
-				
 				List<String> listArgs = Arrays.asList(args);
-				Diretivas.printDebug = listArgs.indexOf("-d") >= 0;
-				
+				Diretivas.printDebug = listArgs.indexOf("-d") >= 0;				
 			}
 
-            // Procedimento:
-            //  Cria População;
-            //  Para cada iteração:
-            //      Seleciona dois genes pais;
-            //      CrossOver;
-            //      Mutação;
-            //      Fitness;
-            //      Seleção dos melhores/Corte;
+			// Procedimento:
+			    //  Cria População;
+			    //  Para cada iteração:
+			    //      Seleciona dois genes pais;
+			    //      CrossOver;
+			    //      Mutação;
+			    //      Fitness;
+			    //      Seleção dos melhores/Corte;
 
-            LerArquivo lerArquivo = new LerArquivo();
-            // cria populacao
-            Populacao populacao = new Populacao(lerArquivo);
-            ArrayList<Disciplina[]> populacaoAtual, populacaoDescendente;
+            		LerArquivo lerArquivo = new LerArquivo();
+            		// cria populacao
+            		Populacao populacao = new Populacao(lerArquivo);
+            		ArrayList<Disciplina[]> populacaoAtual, populacaoDescendente;
 			
 			// processo de evolução da população
-            
 			Fitness fitness = new Fitness();
 			ArrayList<Individuo> individuosPopulacao;
 			Individuo indiv;
 			
-			// Mutacao mutacao = new Mutacao(populacao.populacaoInicial);
 			CrossOver crossOver = new CrossOver();
-			
+			Mutacao mutacao = new Mutacao();
+
 			populacaoAtual = new ArrayList<Disciplina[]>(populacao.populacaoInicial);
 			
 			for(int i = 0; i < quantidadeIteracoes; i++){
-            
 				// crossover
-			   populacaoDescendente = new ArrayList<Disciplina[]>(populacaoAtual);
+				populacaoDescendente = new ArrayList<Disciplina[]>(populacaoAtual);
 			   
-			   crossOver.efetuarCruzamento(populacaoDescendente, lerArquivo.semestres);
-			   populacaoAtual.addAll(populacaoDescendente);
+			   	crossOver.efetuarCruzamento(populacaoDescendente, lerArquivo.semestres);
+			   	populacaoAtual.addAll(populacaoDescendente);
 			   
-               //Mutação
-               
-               //Fitness
-			   
-			   individuosPopulacao = new ArrayList<Individuo>();
-			   
-			   for (int j = 0; j < populacaoAtual.size(); j++) {
+               			//Mutação
+               			mutacao.Mutar(populacaoAtual);
+
+               			//Fitness
+			   	individuosPopulacao = new ArrayList<Individuo>();
+			   	for (int j = 0; j < populacaoAtual.size(); j++) {
 					if (Diretivas.printDebug)
 						System.out.print("Calculando fitness do individuo " + j + "...");
 					
@@ -75,7 +72,6 @@ class Main {
 				}
 				
 				// ordenar pela fitness
-				
 				Collections.sort(individuosPopulacao, new Comparator<Individuo>() {
 					@Override
 					public int compare(Individuo i1, Individuo i2)
@@ -84,16 +80,56 @@ class Main {
 					}
 				});
 				
-			    //Corte
+			    	//Corte
 				populacaoAtual.clear();
 				for (int k = 0; k < 750; k++)
 					populacaoAtual.add(individuosPopulacao.get(k).disciplinas);
 				
-            }
+            		}
 			
-//            if (Diretivas.printDebug)
-//				populacao.imprimeIndividuos();
-
+            		//if (Diretivas.printDebug)
+				//populacao.imprimeIndividuos();
+			
+	
+			for(int i = 1; i <= 5; i++){
+				int k;
+                		if(i == 1) {
+                    			k = 0;
+					System.out.println("\nSala 101");
+					
+					
+                		} else if(i == 2) {
+                    			k = 30;
+					System.out.println("\nSala 102");
+				
+                		} else if(i == 3) {
+                    			k = 60;
+					System.out.println("\nSala 103");
+					
+                		} else if(i == 4) {
+                    			k = 90;
+					System.out.println("\nSala 104");
+					
+                		} else{
+                    			k = 120;
+					System.out.println("\nSala 105");
+					
+                		}		
+								
+				for(int j = k; j < k + 30; j++){
+					if(populacaoAtual.get(0)[j] != null){						
+						
+						System.out.println("Horario " + (j - k) + ": " + populacaoAtual.get(0)[j].codigo + " - " + populacaoAtual.get(0)[j].professor.nome);	
+						
+					}else{
+						System.out.println("Horario " + (j - k) + ": Nada");
+						
+					}
+							
+				}
+								
+				
+			}
         }
 		catch (Exception ex){
             System.out.println(ex.getCause().getMessage());
